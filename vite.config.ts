1a491c7cd573727e7fdd7a238f6e9ea56096d0ba
rihab-ze <rihabzekrioui5@gmail.com>
2025-10-23 16:08:23 +0100

    Migrate the cli to the latest version

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { PluginOption, defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-
+import importMetaUrlPlugin from '@ws-ui/vite-plugins/dist/esbuild-plugin-import-meta-url';
+import monacoEditorPlugin from '@ws-ui/vite-plugins/dist/vite-plugin-monaco-editor';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+import { federation } from '@module-federation/vite';
 import { initProxy } from './proxy.config';
+import { dependencies as deps, app_id } from './package.json';
+
+const exposes = {
+  './components': './src/components/index.tsx',
+};
+
+const isDevEnv = process.env.NODE_ENV === 'development';
 
 const redirect = (opts: { from: string; to: string }): PluginOption => {
   return {
@@ -21,6 +31,51 @@ const redirect = (opts: { from: string; to: string }): PluginOption => {
   };
 };
 
+const getBuildPlugins = () => {
+  if (isDevEnv) {
+    return [monacoEditorPlugin()];
+  }
+
+  return [
+    federation({
+      name: app_id,
+      filename: 'components.js',
+      exposes,
+      library: { type: 'var' },
+      shared: {
+        ...['react', 'react-dom', 'react/jsx-runtime'].reduce(
+          (acc, key) => ({
+            ...acc,
+            [key]: {
+              requiredVersion: deps[key],
+              singleton: true,
+              eager: true,
+            },
+          }),
+          {},
+        ),
+        ...[
+          // @ws-ui
+          '@ws-ui/webform-editor',
+          '@ws-ui/craftjs-core',
+          '@ws-ui/craftjs-layers',
+          '@ws-ui/craftjs-utils',
+          '@ws-ui/shared',
+        ].reduce(
+          (acc, key) => ({
+            ...acc,
+            [key]: {
+              singleton: true,
+              eager: true,
+            },
+          }),
+          {},
+        ),
+      },
+    }),
+  ];
+};
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode = 'local' }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -37,14 +92,40 @@ export default defineConfig(({ mode = 'local' }) => {
         from: '/studio/',
         to: '/',
       }),
+      cssInjectedByJsPlugin({
+        topExecutionPriority: false,
+        jsAssetsFilterFunction: (outputChunk) => {
+          if (outputChunk.name === 'components') {
+            return true;
+          }
+
+          return false;
+        },
+      }),
+      ...getBuildPlugins(),
     ],
     define: {
       'process.env': {},
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        plugins: isDevEnv ? [importMetaUrlPlugin] : [],
+      },
     },
     server: {
       host,
       proxy,
       port: +port,
+    },
+    build: {
+      rollupOptions: {
+        external: ['@ws-ui/code-editor'],
+        output: {},
+      },
+      modulePreload: false,
+      target: 'esnext',
+      minify: false,
+      cssCodeSplit: false,
     },
   };
 });
